b4d14611247834e348db2df14a438224be3c9d7a
Alan King <alan.king@essentialskillz.com>
2016-02-17 11:23:35 +0000

    imgs/screens/script/css for multi feedback

--- lang/eng/4_tooltipsquestions_older_courseware_styles_do_not_use_for_deprecatedregression_testing/custom.js
+++ lang/eng/4_tooltipsquestions_older_courseware_styles_do_not_use_for_deprecatedregression_testing/custom.js
@@ -9,7 +9,7 @@ var nextLessonButtonAlt = 'Launch next lesson';
 var endMessageAssessment = 'Click to start the %linked%';
 var congratulationLinkedAssessmentHeader = 'Risk Assessment';
 var congratulationLinkedSurveyHeader = 'Survey';
-var noOfScreens = 24;
+var noOfScreens = 26;
 var loadBookmarkText = 'Would you like to go to the last bookmarked screen in this lesson?';
 var currentLessonNo = 4;
 
